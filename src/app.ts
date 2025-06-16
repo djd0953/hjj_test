20030f54c7134934f9da1c25526e1d1c467b2f2d
hjj0106 <hjj0106@amicuslex.net>
2025-06-16 11:14:03 +0900

    완료

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express from 'express';
-import fs from 'fs';
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {access} from 'fs/promises'
+import fs, { constants } from 'fs';
+import { GetObjectCommand, HeadObjectCommand, S3Client, ListObjectsV2Command, ListObjectsV2CommandOutput, ListObjectsV2Request } from '@aws-sdk/client-s3';
 import { simpleParser, ParsedMail } from 'mailparser';
 import * as cheerio from 'cheerio';
 import {Document, Element} from 'domhandler'
+import exceljs from 'exceljs'
+import type { WorksheetModel } from 'exceljs';
 
 import CF_ERROR_STATUS from './error'
+import dayjs from 'dayjs';
+import path, { normalize } from 'path';
 
 const email = require('./email.json')
 
 const AWS_ACCESS_KEY_ID= 'AKIA2WUHLMAMMLV3GTR4'
 const AWS_SECRET_ACCESS_KEY='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'
 const AWS_REGION='ap-northeast-2'
-const BUCKET_NAME = 'lawform'
+const BUCKET_NAME = ['lawform', 'cfs.lawform.io']
 const app = express();
 
 const separators: { value: string | RegExp }[] = [
@@ -27,6 +32,84 @@ interface S3RetrieveParams {
     key: string;
 }
 
+interface WorksheetWithModel extends WorksheetModel {
+    rows: {
+        cells: {
+            address: string,
+            type: number,
+            style: {
+                numFmt: string
+            },
+            value?: string | number | {
+                    richText: {
+                        font: {size:number, name:string}[], 
+                        text:string
+                    }[]
+                },
+            result?: string | number | null
+        }[],
+        number: number
+    }[]
+}
+
+interface cellModel {
+    address: string,
+    value: string | number
+    title?: string
+}
+
+interface ExcelDatas {
+    teamName: string | number,
+    contractCategoryName: string | number,
+    manageNumber: string | number,
+    contractName: string | number,
+    categoryName: string | number,
+    subCategoryName: string | number,
+    minorCategoryName: string | number,
+    cfsFileName: string | number,
+    userName: string | number,
+    legalName: string | number,
+    contractDateRange: string | number,
+    isAutoExtend: string | number,
+    customerBusinessName: string | number,
+    referenceUserName: string | number,
+    contractDate: string | number,
+    contractValue: string | number,
+    memo_purpose: string | number,
+    memo_topic: string | number,
+    contract_review_deadline: string | number,
+    etc: string | number
+}
+
+type ExcelDataKeys = keyof ExcelDatas
+
+interface s3ListEntity {
+    id: number,
+    key?: string
+}
+
+const EXCEL_HEAD_TITLE_MAP = [
+    {excelTitle: '팀명', key: 'teamName'},
+    {excelTitle: '항목', key: 'contractCategoryName'},
+    {excelTitle: '관리번호', key: 'manageNumber'},
+    {excelTitle: '계약명', key: 'contractName'},
+    {excelTitle: '계약 대분류', key: 'categoryName'},
+    {excelTitle: '계약 중분류', key: 'subCategoryName'},
+    {excelTitle: '계약 소분류', key: 'minorCategoryName'},
+    {excelTitle: '계약서명(파일명과 동일)', key: 'cfsFileName'},
+    {excelTitle: '담당자(요청자)', key: 'userName'},
+    {excelTitle: '법무 검토 담당자(,로 구분)', key: 'legalName'},
+    {excelTitle: '계약 기간(~로 구분)', key: 'contractDateRange'},
+    {excelTitle: '계약 자동 연장 유무', key: 'isAutoExtend'},
+    {excelTitle: '상대 계약자', key: 'customerBusinessName'},
+    {excelTitle: '참조 수신자', key: 'referenceUserName'},
+    {excelTitle: '계약 예정일', key: 'contractDate'},
+    {excelTitle: '계약 규모(VAT포함)', key: 'contractValue'},
+    {excelTitle: '계약 배경/목적', key: 'memo_purpose'},
+    {excelTitle: '주요 협의사항', key: 'memo_topic'},
+    {excelTitle: '검토 마감기한', key: 'contract_review_deadline'},
+]
+
 const S3RetreiveFileBuffer = async ({ key }: S3RetrieveParams): Promise<{ status: number; Body?: Buffer; message?: unknown }> => {
     const normalizeKey = key;
 
@@ -39,7 +122,7 @@ const S3RetreiveFileBuffer = async ({ key }: S3RetrieveParams): Promise<{ status
     });
 
     const params = {
-        Bucket: BUCKET_NAME || '',
+        Bucket: BUCKET_NAME[0] || '',
         Key: normalizeKey,
     };
 
@@ -59,6 +142,73 @@ const S3RetreiveFileBuffer = async ({ key }: S3RetrieveParams): Promise<{ status
         return { status: 400, message: err };
     }
 };
+
+const S3HeadObject = async ({ key }: S3RetrieveParams): Promise<boolean> => {
+    const normalizeKey = key
+
+    const s3Client = new S3Client({
+        region: AWS_REGION || '',
+        credentials: {
+            accessKeyId: AWS_ACCESS_KEY_ID || '',
+            secretAccessKey: AWS_SECRET_ACCESS_KEY || '',
+        },
+    });
+
+    const params = {
+        Bucket: BUCKET_NAME[1] || '',
+        Key: normalizeKey,
+    }
+
+    const command = new HeadObjectCommand(params)
+
+    try {
+        await s3Client.send(command)
+        return true
+    } catch (err) {
+        console.log(err)
+        return false
+    }
+}
+
+const S3ListObjects = async ({ key }: S3RetrieveParams): Promise<s3ListEntity[]> => {
+
+    const s3Client = new S3Client({
+        region: AWS_REGION || '',
+        credentials: {
+            accessKeyId: AWS_ACCESS_KEY_ID || '',
+            secretAccessKey: AWS_SECRET_ACCESS_KEY || '',
+        },
+    });
+
+    const list = []
+    try {
+        let isTruncated = true;
+        let ContinuationToken = undefined
+
+        while (isTruncated) {
+            const params = {
+                Bucket: BUCKET_NAME[1],
+                Prefix: key,
+                ContinuationToken
+            } as ListObjectsV2Request
+
+            const data = new ListObjectsV2Command(params);
+            const command = await s3Client.send(data);
+            if (command.Contents) {
+                list.push(...command.Contents.map(obj => ({key: obj.Key, id: list.length + 1})))
+            }
+            isTruncated = command.IsTruncated ?? false;
+            ContinuationToken = command.NextContinuationToken
+
+        }
+
+    } catch (err) {
+        console.error('Error list files:', err)
+    }
+    finally {
+        return list
+    }
+}
 
 const settingEmailJson = async (num?: number): Promise<void> => {
     const paths: string[] = [
@@ -139,44 +289,226 @@ class emailHtmlBySeparator {
     }
 }
 
-const start = async () => {
-    // console.log(1);
-    // console.log(1);
+const parsingExcelData = async (): Promise<ExcelDatas[]> => {
+    /* Get Excel Data (Exceljs) */
+    const workbook = new exceljs.Workbook();
+    await workbook.xlsx.readFile('files/clm_list.xlsx');
 
+    const dataSheet = workbook.worksheets.find(x => x.name === 'data');
+    // const dataRows = dataSheet?.getRows(1, dataSheet.actualRowCount) || []
+    const dataModel = dataSheet?.model as WorksheetWithModel;
+
+    let headers: cellModel[] = []
+    const values: cellModel[][] = []
+    for (const row of dataModel.rows) {
+
+        const cells = row.cells.map(x => {
+            let val: string | number = "";
+
+            if (typeof x.value === "object" && x.value?.richText) 
+                val = x.value.richText.map(z => z.text).join('')
+            else if (typeof x.value === "string" || typeof x.value === "number")
+                val = x.value
+            else if (x.value instanceof Date)
+                val = dayjs(x.value).format('YYYY.MM.DD HH:mm:ss')
+            else if (x.result)
+                val = x.result
+
+            return {
+                address: x.address.replace(`${row.number}`, ''),
+                value: val
+            }
+        })
+
+        if (row.number === 1) headers = cells
+        else values.push(cells)
+    }
+
+    const r = values.map(x => {
+        const obj: ExcelDatas = {
+            teamName: "",
+            contractCategoryName: "",
+            manageNumber: "",
+            contractName: "",
+            categoryName: "",
+            subCategoryName: "",
+            minorCategoryName: "",
+            cfsFileName: "",
+            userName: "",
+            legalName: "",
+            contractDateRange: "",
+            isAutoExtend: "",
+            customerBusinessName: "",
+            referenceUserName: "",
+            contractDate: "",
+            contractValue: "",
+            memo_purpose: "",
+            memo_topic: "",
+            contract_review_deadline: "",
+            etc: ""
+        }
+
+        for (const header of headers) {
+            const address = header.address;
+            const key = EXCEL_HEAD_TITLE_MAP.find(y => y.excelTitle === header.value)?.key ?? "etc";
+            obj[key as keyof ExcelDatas] = x.find(cell => cell.address === address)?.value || '' as string | number;
+        }
+
+        return obj
+    })
+
+    return r
+}
+
+const splitYMD = (t: string): {y: string, m: string, d: string} => {
+    const d = t.split(t.indexOf('.') >= 0 ? '.' : '-')
+    
+    return {
+        y: d[0].length < 4 ? `20${d[0].trim()}` : d[0].trim(),
+        m: d[1].trim(),
+        d: d[2] ? d[2].trim() : "01",
+    }
+}
+
+const diffDate = (text: string) => {
+    const splitIndex = text.indexOf('~');
+    const start = splitYMD(text.substring(0, splitIndex));
+    const end = splitYMD(text.substring(splitIndex + 1, text.indexOf('(') >= 0 ? text.indexOf('(') : text.length))
+
+    const startDate = dayjs(`${start.y}.${start.m}.${start.d}`)
+    const endDate = dayjs(`${end.y}.${end.m}.${end.d}`)
+    const diff = endDate.diff(startDate, 'days')
+
+    return {
+        s: startDate.isValid() ? startDate.format('YYYY-MM-DD') : null,
+        e: endDate.isValid() ? endDate.format('YYYY-MM-DD') : null,
+        d: !isNaN(diff) ? diff : null
+    }
+}
+
+const checkMissingFiles = async (data: ExcelDatas[]) => {
+    const defaultPath = `files/contract`
+    const missingFiles = await Promise.all(
+        data.map(async (item) => {
+            const filePath = !item.contractCategoryName ? path.join(defaultPath, item.teamName as string, item.cfsFileName as string) : path.join(defaultPath, item.teamName as string, item.contractCategoryName as string ,item.cfsFileName as string)
+            try {
+                await access(filePath, constants.F_OK)
+                return null
+            }
+            catch {
+                return `${item.teamName}${item.contractCategoryName && `(${item.contractCategoryName})`}: ${item.cfsFileName}`
+            }
+        })
+    )
+
+    await fs.writeFileSync("missing_files.txt", missingFiles.filter(v => v !== null).join('\n'), {encoding: 'utf-8'});
+}
+
+const test = () => {
+    const a = [
+        {
+            team1: [
+                { 
+                    category1: [
+                        { id: 1, name: 'file1' },
+                        { id: 2, name: 'file2' }
+                    ] 
+                }
+            ],
+        },
+        {
+            team2: [
+                    { id: 3, name: 'file3' }
+            ]
+        },
+        { id: 3, name: 'file3' }
+    ]
+
+    
+}
+
+
+const getTreeToS3List = (list: s3ListEntity[], prefix: string) => {
+    const tree: any[] = []
+
+    for (const item of list) {
+        const {id, key} = item
+        if (!key) continue;
+
+        const path = key.replace(prefix, '').split('/');
+        let currentPath = tree
+        for (let i = 0; i < path.length; i++) {
+            if (i === path.length - 1) {
+                const input = {
+                    id,
+                    name: path[i],
+                    normalize: path[i].normalize()
+                }
+
+                currentPath.push(input)
+                continue;
+            }
+
+            const nextPath = currentPath.find(t => t[path[i]])
+            if (!nextPath) {
+                currentPath.push({[path[i].trim()]: []})
+                currentPath = currentPath.find(t => t[path[i].trim()])[path[i].trim()]
+            } else {
+                currentPath = nextPath[path[i].trim()]
+            }
+        }
+    }
+
+    return tree
+}
+
+const start = async () => {
     // const e: string = email[8].textAsHtml;
     // const aa = new emailHtmlBySeparator(e);
     // const bb = aa.splitByDepthIndex();
     // console.log(bb);
 
-    let str = 'export type CF_ERROR_STATUS_INTERFACE = {\n'
-    for (const [k1, o] of Object.entries(CF_ERROR_STATUS)) {
-        str += `\t${k1}: {\n`
+    const a = await parsingExcelData()
+    const prefix = `business_partners_files/deaju/migration_files/contract/`
+    const list = await S3ListObjects({key: prefix})
+    const normalizeList = getTreeToS3List(list, prefix)
 
-        for (const [k2, v] of Object.entries(o)) {
-            switch (k2) {
-                case "code":
-                case "id":
-                    str += `\t\t${k2}: ${v}\n`
-                    break;
-                case "name":
-                case "message":
-                    str += `\t\t${k2}: string\n`
-                    break;
-                default:
-                    str += `\t\t${k2}: ${typeof v}`
-                    break;
-            }
-        }
+    for (let i = 0; i < 1; i++) {
+        const {
+            teamName,
+            contractCategoryName,
+            cfsFileName
+        } = a[i]
 
-        str += `\t},\n`
+        const categoryPath = contractCategoryName ? `/${contractCategoryName}` : ''
+        let path = `${prefix}`;
+
+        const inputPath = `${prefix}${teamName}${categoryPath}${(cfsFileName as string).trim()}`
+        
+        const isIn = await S3HeadObject({key: inputPath})
+        if (!isIn)
+            console.log(inputPath)
     }
 
-    str += `}`
+    // const list = [...new Set(a.map(x => x.categoryName).filter(name => name !== ''))];
+ 
+    // const b1 = "2024.07.01~2025.06.30";
+    // const b2 = "21.05.15~22.05.14(1년)";
+    // const b3 = "2024.06.18~(상호만변경됨)";
+    // const b4 = "2024.09.25 ~ 2025.3.25";
+    // const aa = diffDate(b4)
 
-    console.log(str)
+    // const c = dayjs(aa.e)
+    // const c2 = dayjs()
+
+    // console.log(c > c2)
+
+    console.log(1);
+    console.log(1);
 };
 
 start();
 // settingEmailJson(10);
+// test();
 
 app.listen(8888, () => {});
